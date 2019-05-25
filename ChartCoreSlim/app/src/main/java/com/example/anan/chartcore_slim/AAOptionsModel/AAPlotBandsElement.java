98bfdef4e1129737dd59290fa0d3bc8044a53632
An An <2236368544@qq.com>
2019-05-25 14:27:56 +0800

    Add more useful properties for AAPlotBandsElement

--- ChartCoreSlim/app/src/main/java/com/example/anan/chartcore_slim/AAOptionsModel/AAPlotBandsElement.java
+++ ChartCoreSlim/app/src/main/java/com/example/anan/chartcore_slim/AAOptionsModel/AAPlotBandsElement.java
@@ -1,10 +1,14 @@
 package com.example.anan.chartcore_slim.AAOptionsModel;
 
 public class AAPlotBandsElement {
-
     private Float from;
     private Float to;
     private Object color;
+    private String borderColor;
+    private Float borderWidth;
+    private String className;
+    private AALabel label;
+    private Integer index;
 
     public AAPlotBandsElement from(Float prop) {
         from = prop;
@@ -21,5 +25,29 @@ public class AAPlotBandsElement {
         return this;
     }
 
+    public AAPlotBandsElement borderColor(String prop) {
+        borderColor = prop;
+        return this;
+    }
+
+    public AAPlotBandsElement borderWidth(Float prop) {
+        borderWidth = prop;
+        return this;
+    }
+
+    public AAPlotBandsElement className(String prop) {
+        className = prop;
+        return this;
+    }
+
+    public AAPlotBandsElement label(AALabel prop) {
+        label = prop;
+        return this;
+    }
+
+    public AAPlotBandsElement index(Integer prop) {
+        index = prop;
+        return this;
+    }
 
 }
