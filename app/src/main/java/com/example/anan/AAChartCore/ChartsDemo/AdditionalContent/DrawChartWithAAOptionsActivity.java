084d0154acaab54bbdd94ccaedcbfc6b131fab86
An An <2236368544@qq.com>
2020-02-01 23:23:25 +0800

    Format code

--- app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/AdditionalContent/DrawChartWithAAOptionsActivity.java
+++ app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/AdditionalContent/DrawChartWithAAOptionsActivity.java
@@ -403,10 +403,10 @@ public class DrawChartWithAAOptionsActivity extends AppCompatActivity {
                 .dataLabelsEnabled(false)
                 .markerRadius(0f)
                 .series(new AASeriesElement[] {
-                                new AASeriesElement()
-                                        .name("Berlin Hot")
-                                        .color(AAGradientColor.mysticMauveColor())
-                                        .data(new Object[]{7.0, 6.9, 2.5, 14.5, 18.2, 21.5, 5.2, 26.5, 23.3, 45.3, 13.9, 9.6})
+                        new AASeriesElement()
+                                .name("Berlin Hot")
+                                .color(AAGradientColor.mysticMauveColor())
+                                .data(new Object[]{7.0, 6.9, 2.5, 14.5, 18.2, 21.5, 5.2, 26.5, 23.3, 45.3, 13.9, 9.6})
                                 ,
                         }
                 );
@@ -441,10 +441,10 @@ public class DrawChartWithAAOptionsActivity extends AppCompatActivity {
                 .dataLabelsEnabled(false)
                 .markerRadius(0f)
                 .series(new AASeriesElement[] {
-                                new AASeriesElement()
-                                        .name("Berlin Hot")
-                                        .color(AAGradientColor.deepSeaColor())
-                                        .data(new Object[]{7.0, 6.9, 2.5, 14.5, 18.2, 21.5, 5.2, 26.5, 23.3, 45.3, 13.9, 9.6})
+                        new AASeriesElement()
+                                .name("Berlin Hot")
+                                .color(AAGradientColor.deepSeaColor())
+                                .data(new Object[]{7.0, 6.9, 2.5, 14.5, 18.2, 21.5, 5.2, 26.5, 23.3, 45.3, 13.9, 9.6})
                                 ,
                         }
                 );
