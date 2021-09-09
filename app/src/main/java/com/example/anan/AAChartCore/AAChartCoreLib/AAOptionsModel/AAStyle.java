19ee39a22ce52b538db7128dec3ba688abf9fab7
An An <2236368544@qq.com>
2021-09-09 18:20:30 +0800

    🚀 Fix #107 => Support all Highcharts CSSObject properties for AAStyle

--- app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAOptionsModel/AAStyle.java
+++ app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAOptionsModel/AAStyle.java
@@ -1,23 +1,116 @@
 package com.example.anan.AAChartCore.AAChartCoreLib.AAOptionsModel;
 
 public class AAStyle {
+
+    public String background;
+    public String backgroundColor;
+    public String border;
+    public String borderRadius;
     public String color;
+    public String cursor;
+    public String fontFamily;
     public String fontSize;
     public String fontWeight;
+    public Number height;
+    public Number lineWidth;
+    public Number opacity;
+    public String padding;
+    public String pointerEvents;
+    public String position;
+    public String textAlign;
+    public String textDecoration;
     public String textOutline;
+    public String textOverflow;
+    public String top;
+    public String transition;
+    public String whiteSpace;
+    public Number width;
+
+    public AAStyle background(String prop) {
+        background = prop;
+        return this;
+    }
+
+    public AAStyle backgroundColor(String prop) {
+        backgroundColor = prop;
+        return this;
+    }
+
+    public AAStyle border(String prop) {
+        border = prop;
+        return this;
+    }
+
+    public AAStyle borderRadius(Number prop) {
+        borderRadius = prop + "px";
+        return this;
+    }
 
     public AAStyle color(String prop) {
         color = prop;
         return this;
     }
 
-    public AAStyle fontSize(Float prop) {
+    public AAStyle cursor(String prop) {
+        cursor = prop;
+        return this;
+    }
+
+    public AAStyle fontFamily(String prop) {
+        fontFamily = prop;
+        return this;
+    }
+
+    public AAStyle fontSize(Number prop) {
         fontSize = prop + "px";
         return this;
     }
 
+
     public AAStyle fontWeight(String prop) {
         fontWeight = prop;
+        return this;
+    }
+
+
+
+    public AAStyle height(Number prop) {
+        height = prop;
+        return this;
+    }
+
+    public AAStyle lineWidth(Number prop) {
+        lineWidth = prop;
+        return this;
+    }
+
+    public AAStyle opacity(Number prop) {
+        opacity = prop;
+        return this;
+    }
+
+    public AAStyle padding(String prop) {
+        padding = prop;
+        return this;
+    }
+
+    public AAStyle pointerEvents(String prop) {
+        pointerEvents = prop;
+        return this;
+    }
+
+    public AAStyle position(String prop) {
+        position = prop;
+        return this;
+    }
+
+    public AAStyle textAlign(String prop) {
+        textAlign = prop;
+        return this;
+    }
+
+    public AAStyle textDecoration(String prop) {
+        textDecoration = prop;
         return this;
     }
 
@@ -25,6 +118,35 @@ public class AAStyle {
         textOutline = prop;
         return this;
     }
+
+    public AAStyle textOverflow(String prop) {
+        textOverflow = prop;
+        return this;
+    }
+
+    public AAStyle top(String prop) {
+        top = prop;
+        return this;
+    }
+
+    public AAStyle transition(String prop) {
+        transition = prop;
+        return this;
+    }
+
+    public AAStyle whiteSpace(String prop) {
+        whiteSpace = prop;
+        return this;
+    }
+
+    public AAStyle width(Number prop) {
+        width = prop;
+        return this;
+    }
+
+
+
+
 
     public static AAStyle style(
             String color
