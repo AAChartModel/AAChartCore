1ee3662648f1c869950f5ff99c4789e0db8e5dc7
An An <2236368544@qq.com>
2022-03-22 16:23:25 +0800

    Update CustomStyleChartComposer.java

--- app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/chartcomposer/CustomStyleChartComposer.java
+++ app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/chartcomposer/CustomStyleChartComposer.java
@@ -1208,7 +1208,7 @@ public class CustomStyleChartComposer {
     public static AAChartModel noMoreGroupingAndOverlapEachOtherColumnChart() {
         return new AAChartModel()
                 .chartType(AAChartType.Column)
-                .categories(new String[]{"11/23","11/24", "11/25","11/26","11/27","11/28","11/29"})
+                .categories(new String[]{"11/23", "11/24", "11/25", "11/26", "11/27", "11/28", "11/29"})
 //            .yAxisTickPositions([0, 10, 20, 30, 40, 50])
                 .yAxisMax(50f)
                 .yAxisMin(0f)
@@ -1279,47 +1279,71 @@ public class CustomStyleChartComposer {
 
     //https://github.com/AAChartModel/AAChartKit-Swift/issues/323
     //https://github.com/AAChartModel/AAChartKit-Swift/issues/346
+    //https://github.com/highcharts/rounded-corners
     public static AAChartModel freeStyleRoundedCornersStackingColumnChart() {
         return new AAChartModel()
                 .chartType(AAChartType.Column)
-                .stacking(AAChartStackingType.Normal)
+                .stacking(AAChartStackingType.Percent)
                 .title("Free-Style Rounded Corners Stacking Column Chart")
+                .xAxisVisible(false)
+                .yAxisGridLineWidth(0f)
                 .colorsTheme(new Object[]{
                         AAGradientColor.linearGradient(AAColor.rgbColor(128, 255, 165), AAColor.rgbColor(1  , 191, 236)),
                         AAGradientColor.linearGradient(AAColor.rgbColor(0  , 221, 255), AAColor.rgbColor(77 , 119, 255)),
                         AAGradientColor.linearGradient(AAColor.rgbColor(55 , 162, 255), AAColor.rgbColor(116, 21 , 219)),
                         AAGradientColor.linearGradient(AAColor.rgbColor(255, 0  , 135), AAColor.rgbColor(135, 0  , 157)),
-                        AAGradientColor.linearGradient(AAColor.rgbColor(255, 191, 0  ), AAColor.rgbColor(224, 62 , 76 ))
+                        AAGradientColor.linearGradient(AAColor.rgbColor(255, 191, 0  ), AAColor.rgbColor(224, 62 , 76 )),
+                        AAGradientColor.PixieDust,
+                        AAGradientColor.SweetDream,
+                        AAGradientColor.LusciousLime,
+                        AAGradientColor.WroughtIron,
                 })
                 .series(new AASeriesElement[]{
                         new AASeriesElement()
-                                .name("Tokyo Hot")
                                 .borderRadiusTopLeft("50%")
                                 .borderRadiusTopRight("50%")
                                 .data(new Object[]{2.10, 2.54, 2.78, 3.62, 4.41, 4.09, 3.83, 4.47, 4.20, 3.94, 3.80, 3.58, 3.19, 4.30, 3.69, 3.52, 3.02, 3.30}),
 
                         new AASeriesElement()
-                                .name("Berlin Hot")
                                 .borderRadiusBottomLeft("50%")
                                 .borderRadiusBottomRight("50%")
                                 .data(new Object[]{1.56, 1.91, 2.45, 3.87, 3.24, 4.90, 4.61, 4.10, 4.17, 3.85, 4.17, 3.46, 3.46, 3.55, 3.50, 4.13, 2.58, 2.28}),
 
                         new AASeriesElement()
-                                .name("Beijing Hot")
                                 .borderRadiusTopLeft("50%")
                                 .borderRadiusBottomRight("50%")
                                 .data(new Object[]{1.16, 1.67, 2.64, 2.86, 3.00, 3.21, 4.14, 4.07, 3.68, 3.11, 3.41, 3.25, 3.32, 3.07, 3.92, 3.05, 2.18, 3.24}),
 
                         new AASeriesElement()
-                                .name("London Hot")
                                 .borderRadiusTopRight("50%")
                                 .borderRadiusBottomRight("50%")
                                 .data(new Object[]{5.59, 3.09, 4.09, 6.14, 5.33, 6.05, 5.71, 6.22, 6.56, 4.75, 5.27, 6.02, 5.22, 5.77, 6.19, 5.68, 4.33, 5.48}),
 
                         new AASeriesElement()
-                                .name("NewYork Hot")
                                 .borderRadius(20f)
-                                .data(new Object[]{2.10, 2.54, 2.78, 3.62, 4.41, 4.09, 3.83, 4.47, 4.20, 3.94, 3.80, 3.58, 3.19, 4.30, 3.69, 3.52, 3.02, 3.30})
+                                .data(new Object[]{2.10, 2.54, 2.78, 3.62, 4.41, 4.09, 3.83, 4.47, 4.20, 3.94, 3.80, 3.58, 3.19, 4.30, 3.69, 3.52, 3.02, 3.30}),
+
+                        new AASeriesElement()
+                                .borderRadiusTopLeft("50%")
+                                .borderRadiusBottomLeft("50%")
+                                .data(new Object[]{5.59, 3.09, 4.09, 6.14, 5.33, 6.05, 5.71, 6.22, 6.56, 4.75, 5.27, 6.02, 5.22, 5.77, 6.19, 5.68, 4.33, 5.48}),
+
+                        new AASeriesElement()
+                                .borderRadiusTopRight("50%")
+                                .borderRadiusBottomLeft("50%")
+                                .data(new Object[]{1.16, 1.67, 2.64, 2.86, 3.00, 3.21, 4.14, 4.07, 3.68, 3.11, 3.41, 3.25, 3.32, 3.07, 3.92, 3.05, 2.18, 3.24}),
+
+                        new AASeriesElement()
+                                .borderRadiusBottomLeft("50%")
+                                .borderRadiusBottomRight("50%")
+                                .data(new Object[]{2.10, 2.54, 2.78, 3.62, 4.41, 4.09, 3.83, 4.47, 4.20, 3.94, 3.80, 3.58, 3.19, 4.30, 3.69, 3.52, 3.02, 3.30}),
+
+                        new AASeriesElement()
+                                .borderRadiusTopLeft("50%")
+                                .borderRadiusTopRight("50%")
+                                .data(new Object[]{1.56, 1.91, 2.45, 3.87, 3.24, 4.90, 4.61, 4.10, 4.17, 3.85, 4.17, 3.46, 3.46, 3.55, 3.50, 4.13, 2.58, 2.28}),
+
+
                 });
     }
 
