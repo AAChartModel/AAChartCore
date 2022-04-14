ade93a6cc8723d8bf965174d582c6102b8fcc1fb
An An <2236368544@qq.com>
2022-04-14 19:31:39 +0800

    Create AAChartAxisType.java

---
+++ app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAChartEnum/AAChartAxisType.java
@@ -0,0 +1,8 @@
+package com.example.anan.AAChartCore.AAChartCoreLib.AAChartEnum;
+
+public interface AAChartAxisType {
+    String linear      = "linear";
+    String logarithmic = "logarithmic";
+    String datetime    = "datetime";
+    String category    = "category";
+}
