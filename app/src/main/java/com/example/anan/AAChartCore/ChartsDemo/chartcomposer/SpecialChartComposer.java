22e3155a6f7bd7045cd6f151b123c2592468a910
An An <2236368544@qq.com>
2022-07-02 16:12:55 +0800

    Update SpecialChartComposer.java

--- app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/chartcomposer/SpecialChartComposer.java
+++ app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/chartcomposer/SpecialChartComposer.java
@@ -911,7 +911,6 @@ public class SpecialChartComposer {
 
     public static AAChartModel configureErrorbarChart() {
         return new AAChartModel()
-                .yAxisTitle("")
                 .categories(new String[]{
                         "一月", "二月", "三月", "四月", "五月", "六月",
                         "七月", "八月", "九月", "十月", "十一月", "十二月"
