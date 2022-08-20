307dbcc5be872585fee1181834239a81170ac2d6
An An <2236368544@qq.com>
2022-08-20 18:22:25 +0800

    Update AALang.java

--- app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAOptionsModel/AALang.java
+++ app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAOptionsModel/AALang.java
@@ -1,8 +1,26 @@
 package com.example.anan.AAChartCore.AAChartCoreLib.AAOptionsModel;
 
 public class AALang {
+    public String noData;
+    public Integer numericSymbolMagnitude;
+    public String[] numericSymbols;
     public String resetZoom;
     public String thousandsSep;
+
+    public AALang noData(String prop) {
+        noData = prop;
+        return this;
+    }
+
+    public AALang numericSymbolMagnitude(Integer prop) {
+        numericSymbolMagnitude = prop;
+        return this;
+    }
+
+    public AALang numericSymbols(String[] prop) {
+        numericSymbols = prop;
+        return this;
+    }
 
     public AALang resetZoom(String prop) {
         resetZoom = prop;
