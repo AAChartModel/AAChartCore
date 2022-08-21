a90a4279cae6ec8b8191070802743ce46e71ddd0
An An <2236368544@qq.com>
2022-08-21 10:13:11 +0800

    Create AAPoint.java

---
+++ app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAOptionsModel/AAPoint.java
@@ -0,0 +1,10 @@
+package com.example.anan.AAChartCore.AAChartCoreLib.AAOptionsModel;
+
+public class AAPoint {
+    public AAPointEvents events;
+
+    public AAPoint events(AAPointEvents prop) {
+        events = prop;
+        return this;
+    }
+}
