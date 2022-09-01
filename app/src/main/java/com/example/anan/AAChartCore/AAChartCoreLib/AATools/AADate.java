1b1dc35c3c6ba8f445bce78525137dff07cf2691
An An <2236368544@qq.com>
2022-09-01 21:53:05 +0800

    Create AADate.java

---
+++ app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AATools/AADate.java
@@ -0,0 +1,23 @@
+package com.example.anan.AAChartCore.AAChartCoreLib.AATools;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+
+public class AADate {
+    //   get UTC number from date
+    public static long AADateUTC(int year, int month, int day) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = null;
+        try {
+            date = sdf.parse(year + "-" + month + "-" + day);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date.getTime();
+    }
+}
