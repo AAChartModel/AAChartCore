7540558efd6a029f250b712794c63c6fb6c71953
zhaoyan.li <2236368544@qq.com>
2022-10-16 14:52:05 +0800

    Update JSFunctionForAAOptionsActivity.java

--- app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/AdditionalContent/JSFunctionForAAOptionsActivity.java
+++ app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/AdditionalContent/JSFunctionForAAOptionsActivity.java
@@ -17,10 +17,10 @@ public class JSFunctionForAAOptionsActivity extends AppCompatActivity {
 
     AAOptions chartConfigurationWithSelectedIndex(String chartType) {
         switch (chartType) {
-            case "0": return customDoubleXAxesChart();//自定义双 X 轴图表
-            case "1": return disableColumnChartUnselectEventEffectBySeriesPointEventClickFunction();//禁用柱状图图表的选中状态
-            case "2": return customizeEveryDataLabelSinglelyByDataLabelsFormatter();//自定义每个数据点的数据标签内容
-            case "3": return configureColorfulDataLabelsForPieChart();//为饼图图表配置多彩的数据标签
+            case "customDoubleXAxesChart": return customDoubleXAxesChart();//自定义双 X 轴图表
+            case "disableColumnChartUnselectEventEffectBySeriesPointEventClickFunction": return disableColumnChartUnselectEventEffectBySeriesPointEventClickFunction();//禁用柱状图图表的选中状态
+            case "customizeEveryDataLabelSinglelyByDataLabelsFormatter": return customizeEveryDataLabelSinglelyByDataLabelsFormatter();//自定义每个数据点的数据标签内容
+            case "configureColorfulDataLabelsForPieChart": return configureColorfulDataLabelsForPieChart();//为饼图图表配置多彩的数据标签
             default:
                 return customDoubleXAxesChart();
         }
