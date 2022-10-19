1d638a3253ca15a8cf650e00a6669fb5369b0f90
An An <2236368544@qq.com>
2022-10-19 15:48:35 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/MainContent/MainActivity.java
+++ app/src/main/java/com/example/anan/AAChartCore/ChartsDemo/MainContent/MainActivity.java
@@ -119,7 +119,10 @@ public class MainActivity extends AppCompatActivity {
                     "customLineChartWithColorfulMarkersAndLines---彩色连接点和连接线的折线图📈",
                     "customLineChartWithColorfulMarkersAndLines2---彩色连接点和连接线的多组折线的折线图📈",
                     "drawLineChartWithPointsCoordinates---通过点坐标来绘制折线图",
-                    "configureSpecialStyleColumnForNegativeDataMixedPositiveData---为正负数混合的柱形图自定义特殊样式效果"
+                    "configureSpecialStyleColumnForNegativeDataMixedPositiveData---为正负数混合的柱形图自定义特殊样式效果",
+                    "configureMultiLevelStopsArrGradientColorAreasplineMixedLineChart---多层次半透明渐变效果的曲线填充图混合折线图📈",
+                    "connectNullsForSingleAASeriesElement---为单个 AASeriesElement 单独设置是否断点重连",
+
             },
             /*使用AAOptions绘制图表*/
             {
@@ -354,6 +357,9 @@ public class MainActivity extends AppCompatActivity {
                     "customLineChartWithColorfulMarkersAndLines2",
                     "drawLineChartWithPointsCoordinates",
                     "configureSpecialStyleColumnForNegativeDataMixedPositiveData",
+                    "configureMultiLevelStopsArrGradientColorAreasplineMixedLineChart",
+                    "connectNullsForSingleAASeriesElement",
+
             },
 
             {
