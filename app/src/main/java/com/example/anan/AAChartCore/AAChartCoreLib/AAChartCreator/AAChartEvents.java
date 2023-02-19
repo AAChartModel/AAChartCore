bdb0f8299c347d021719d124dbe4bcb3b4ede9e5
An An <2236368544@qq.com>
2023-02-19 16:21:46 +0800

    Update AAChartEvents.java

--- app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAChartCreator/AAChartEvents.java
+++ app/src/main/java/com/example/anan/AAChartCore/AAChartCoreLib/AAChartCreator/AAChartEvents.java
@@ -4,11 +4,23 @@ import com.example.anan.AAChartCore.AAChartCoreLib.AATools.AAJSStringPurer;
 
 public class AAChartEvents {
     public String load;
+    public String redraw;
+    public String render;
     public String selection;
 
 
     public AAChartEvents load(String prop) {
         load = AAJSStringPurer.pureAnonymousJSFunctionString(prop);
+        return this;
+    }
+
+    public AAChartEvents redraw(String prop) {
+        redraw = AAJSStringPurer.pureAnonymousJSFunctionString(prop);
+        return this;
+    }
+
+    public AAChartEvents render(String prop) {
+        render = AAJSStringPurer.pureAnonymousJSFunctionString(prop);
         return this;
     }
 
